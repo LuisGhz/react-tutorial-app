32be3ae3891c3a6e1e5e75231cb1846cccdf84dc
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-16 14:21:48 -0500

    Create calculateWinner function Show message depending of calculateWinner result

--- src/index.js
+++ src/index.js
@@ -40,7 +40,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) status = `Winner: ${ winner }`;
+    else status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
 
     return (
       <div>
@@ -87,3 +91,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], // First vertical line
+    [3, 4, 5], // Second vertical line
+    [6, 7, 8], // Third vertical line
+    [0, 3, 6], // First horizontal line
+    [1, 4, 7], // Second horizontal line
+    [2, 5, 8], // Third horizonal line
+    [0, 4, 8], // \ line
+    [2, 4, 6], // / line
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if ( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) return squares;
+  }
+
+  return null;
+}
