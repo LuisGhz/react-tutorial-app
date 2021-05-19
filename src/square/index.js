7e8af205882e53c92997b58eec0160db8b1f2228
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-19 15:37:14 -0500

    Move function component Square to its own file

---
+++ src/square/index.js
@@ -0,0 +1,11 @@
+export function Square(props) {
+  let value;
+  if (!props.winner) value = props.value;
+  else value = <span className="winner">{ props.value }</span>
+
+  return (
+    <button className="square" onClick={ props.onClick }>
+      { value }
+    </button>
+  );
+}
