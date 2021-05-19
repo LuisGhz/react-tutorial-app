4f75c19c05ee2fa3c9d5cf7b7c15f9f0f351f4f8
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-19 15:40:40 -0500

    Import Board class instead of Square

--- src/index.js
+++ src/index.js
@@ -1,41 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Square } from './square/index';
-
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square 
-        key={ String(i) }
-        winner={ this.props.winner.includes(i) }
-        value={ this.props.squares[i] }
-        onClick={ () => this.props.onClick(i) }
-      />
-    );
-  }
-
-  render() {
-    const maxSquares = 3;
-    let x = 0;
-    let row = [];
-    
-    for(let r = 0; r < maxSquares; r++) {
-        let column = [];
-
-        for (let c = 0; c < maxSquares; c++) column.push(this.renderSquare(x++));
-
-        row.push( <div key={ String(r) } className="board-row" >{ column }</div> );
-      };
-
-    return (
-      <div>
-        { row }
-      </div>
-    );
-  }
-}
+import { Board } from './board/index';
 
 class Game extends React.Component {
 
