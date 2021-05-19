0c8da3d2ad4916335fc0939bb1a0ab4b82bf1769
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-19 15:49:13 -0500

    Delete unnecessary comments

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Game } from './game/index';
 
-// ========================================
-
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
